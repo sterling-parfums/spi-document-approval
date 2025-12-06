fe6e58e68ac947530247505e454e07f970789899
Mustafa Salim <73708902+Trustafa@users.noreply.github.com>
2025-12-06 16:25:36 +0400

    feat: auto-login if user is already logged in

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
-import LogInPage from '@/app/_screens/LogInScreen';
+import LogInScreen from '@/app/_screens/LogInScreen';
+import { findLoggedInUser } from '@/app/api/_services/auth.service';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
-  return <LogInPage />;
+export default async function LoginPage() {
+  const user = await findLoggedInUser();
+
+  if (user) {
+    redirect('/dashboard');
+  }
+
+  return <LogInScreen />;
 }
